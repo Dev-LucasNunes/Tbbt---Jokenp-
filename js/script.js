cefaa08ac03ead4be8273b8046c86f3dbaf9f4ac
Lucas Nunes <dev.lucasnunes@gmail.com>
2022-12-13 21:14:38 -0300

    fazendo lógica do jogo

--- js/script.js
+++ js/script.js
@@ -3,6 +3,38 @@ const buttons2 = [...document.querySelectorAll(".buttons2 button")];
 const player1 = document.querySelector(".player1");
 const cpu = document.querySelector(".cpu");
 let images = ["rock", "scissors", "paper", "lizard", "spock"];
+let botao = document.querySelector(".check");
+
+function check() {
+  if (
+    player1.style.backgroundImage == buttons[0].style.backgroundImage &&
+    cpu.style.backgroundImage == buttons2[0].style.backgroundImage
+  ) {
+    alert("empate");
+  } else if (
+    player1.style.backgroundImage == buttons[0].style.backgroundImage &&
+    cpu.style.backgroundImage == buttons2[1].style.backgroundImage
+  ) {
+    alert("Ganhou");
+  } else if (
+    player1.style.backgroundImage == buttons[0].style.backgroundImage &&
+    cpu.style.backgroundImage == buttons2[2].style.backgroundImage
+  ) {
+    alert("Perdeu");
+  } else if (
+    player1.style.backgroundImage == buttons[0].style.backgroundImage &&
+    cpu.style.backgroundImage == buttons2[3].style.backgroundImage
+  ) {
+    alert("Pedra esmaga lagarto");
+  } else if (
+    player1.style.backgroundImage == buttons[0].style.backgroundImage &&
+    cpu.style.backgroundImage == buttons2[4].style.backgroundImage
+  ) {
+    alert("spock vaporiza pedra");
+  }
+  player1.style.backgroundImage = "";
+  cpu.style.backgroundImage = "";
+}
 
 buttons.forEach((el, i) => {
   el.addEventListener("click", () => {
@@ -11,9 +43,9 @@ buttons.forEach((el, i) => {
 });
 
 buttons2.forEach((el, i) => {
-  el.addEventListener("click", () => {
+  el.addEventListener("click", (e) => {
     cpu.style.backgroundImage = `url(imagens/${images[i]}.png)`;
   });
 });
 
-function check() {}
+botao.addEventListener("click", check);
